2967d1573c828de5068e672cee73d90c8b19662d
chibujax <chibujax@gmail.com>
2017-02-19 10:09:00 +0100

    Correct indentation problem caused by notepad

--- app/min_max/minmax.js
+++ app/min_max/minmax.js
@@ -1,16 +1,16 @@
 exports.findMinMax = function(myValues) {
-	var retValue = [];
-	if (Array.isArray(myValues) === false) {
-		retValue[0] = "Not an array";
-		return retValue;
-	}
-	var max = Math.max.apply(null, myValues); 
-	var min = Math.min.apply(null, myValues);
-	if(max == min) {
-		retValue[0] = min;
-	} else {
-		retValue[0] = min;
-		retValue[1] = max;
-	}
-	return retValue;
+  var retValue = [];
+  if (Array.isArray(myValues) === false) {
+    retValue[0] = "Not an array";
+    return retValue;
+  }
+  var max = Math.max.apply(null, myValues); 
+  var min = Math.min.apply(null, myValues);
+  if(max == min) {
+    retValue[0] = min;
+  } else {
+    retValue[0] = min;
+    retValue[1] = max;
+  }
+  return retValue;
 };
